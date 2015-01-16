8a913d9f52fe55d28c8daa26332f923594ad695d
Connor <connorbfranklin@gmail.com>
2015-01-16 12:52:11 -0500

    sticky nav

---
+++ js/assets.js
@@ -0,0 +1,30 @@
+//STICKY NAV THAT DOESN'T SUCK
+$.fn.extend({
+    sticky: function(){
+        var offsetTop = $(this).offset().top;
+        var offsetLeft = $(this).offset().left;
+        var width = $(this).width();
+        var position = $(window).scrollTop();
+        var el = $(this)
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > offsetTop) {
+                el.attr('style', 'position:fixed;top:0px;margin:0 auto;width:100%'); /**/
+            } else {
+                el.attr('style', '');
+            }
+        });
+    },
+    stickyFixed: function(){
+        var offsetTop = $(this).css('top');
+        var width = $(this).width();
+        var position = $(window).scrollTop();
+        var el = $(this)
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > offsetTop) {
+                el.attr('style', 'position:fixed;top:'+offsetTop+';left:0px;width:' + width + 'px');
+            } else {
+                el.attr('style', '');
+            }
+        });  
+    }
+});
