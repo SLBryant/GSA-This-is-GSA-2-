92dd4577b1ee0872ac55629405879509ecdda0ca
Connor Franklin <connorbfranklin@gmail.com>
2015-01-21 16:41:22 -0500

    working auto-scroll navigation

--- js/app.js
+++ js/app.js
@@ -3,4 +3,14 @@ $(function(){
 	setSectionHeights();
 	setWaypoints();
 	gsaHeader();
+	navigation();
 });
+
+function navigation(){
+	$('nav a').on('click',function(){
+		var sectionName = $(this).attr('data-section');
+		$('html, body').animate({
+		    scrollTop: $('#'+sectionName).offset().top - 60
+		}, 1000);
+	});
+}
