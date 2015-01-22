9d4c301276c4c4f88af10fa95eb5dbb1b5fcb101
Adam Pflantzer <apflantzer@gmail.com>
2015-01-22 16:01:25 -0500

    lightbox and carousel functionality (no styles)

--- js/app.js
+++ js/app.js
@@ -8,6 +8,49 @@ $(function(){
 		setSectionHeights();
 		updateNavPositionLeft();
 	});
+
+    // Carousel
+    $('.carousel').carousel({
+        interval : false
+    });
+
+
+    var carouselID = $('#our-priorities-carousel');
+    var numberOfItems = carouselID.find('.item').length;
+    var eqValue = parseInt(numberOfItems) - 1;
+        carouselID.on('slid.bs.carousel', function () {
+        var firstSlide = $(this).find('.item:eq(0)');
+        var lastSlide = $(this).find('.item:eq('+eqValue+')');
+        // check if it's the first slide
+        if(firstSlide.hasClass('active')) {
+            $(this).find('.left').css('display','none');
+        } else {
+            $(this).find('.left').css('display','block');
+        }
+        // check if it's the last slide
+        if(lastSlide.hasClass('active')) {
+            $(this).find('.right').css('display','none');
+        } else {
+            $(this).find('.right').css('display','block');
+        }
+    });
+
+    // Modal
+    $('#modalView').on('shown.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var file = button.attr('data-source');
+           $('#content-catcher').load('modals/'+file+'.html', function() {
+               $('#modal-loading').fadeOut(300);
+           });
+    });
+
+    $('#modalView').on('hidden.bs.modal', function (event) {
+        $('#content-catcher').empty();
+        $('<img>').attr('id','modal-loading').attr('src','images/loading.gif').appendTo('#content-catcher');
+    });
+
+
+
 });
 
 function navigation(){
