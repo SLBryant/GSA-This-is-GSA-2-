5788305e85985bad963e38e42c38be256e5a6ec7
Connor Franklin <connorbfranklin@gmail.com>
2015-03-13 14:38:17 -0400

    defining gaCrumb

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ if(!GSA){
     var GSA = {}
 }
 var gaDimensions = {};
+var gaCrumb = window.location.href.split('gsa.gov')[1];
 var ieUserAgent = {
     init: function () {
         // Get the user agent string
